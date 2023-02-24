4dce179aacb4e1fd23efea80bd5f224c3494a906
stanleya1293 <arden.stanley@yahoo.com>
2023-02-24 16:26:02 -0600

    changed some stuff

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const canvas = $('gameCanvas');
-const gl = canvas.getContext('webgl2');
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+ctx.font = "30px Verdana";
+ctx.strokeStyle = "Purple";
+ctx.strokeText("CLICK TO START", "450px", "900px");
 
-if (!gl) {
-    alert("Not supported by browser");
-}
+
+
